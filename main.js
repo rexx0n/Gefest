4e953d054a433dee462366de37737c2d65f848b5
rexx0n <alexey.mironenko46@gmail.com>
2022-07-19 15:22:15 +0300

    working with a table

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 let products = document.querySelectorAll(".products__item div")
+products.forEach((product) => {
+    product.addEventListener("click", function (e) {
+        let id = this.id
+        let info = document.querySelector(".products__id")
+        let productInfo = info.querySelector(`.${id}`)
+        if(productInfo.classList.contains("products__info--none")){
+            productInfo.classList.remove('products__info--none')
+            productInfo.classList.add('products__info')
+        }
+        else {
+            productInfo.classList.add("products__info--none");
+        }
+        
+
+    })
+})
