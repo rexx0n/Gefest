a1e9c18f206a84f84bf60b30bc641a9d4f239f9c
rexx0n <alexey.mironenko46@gmail.com>
2022-07-21 18:02:13 +0300

    bug fixing

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-let products = document.querySelectorAll(".products__item div");
-products.forEach((product) => {
-  product.addEventListener("click", function (e) {
-    let id = this.id;
-    let info = document.querySelector(".products__id");
-    let infos = document.querySelector('.products__id div')
-    let productInfo = info.querySelector(`.${id}`);
-    if (productInfo.classList.contains("products__info--none")) {
-      productInfo.classList.remove("products__info--none");
-      productInfo.classList.add("products__info");
-    } else {
-      productInfo.classList.add("products__info--none");
-    }
-    infos.forEach((e) => {
-      if (e.classList.contains('products__info')) {
-        e.classList.add("products__info--none")
-        e.classList.remove('products__info')
-
-      }
+let productsItems = document.querySelectorAll(".products__item");
+productsItems.forEach((product) => {
+  product.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelectorAll(".products__id > div").forEach((el) => {
+      el.classList.add("products__info--none");
     });
+    let info = document.querySelector(".products__id");
+    let productInfo = info.querySelector(`.${this.id}`);
+    productInfo.classList.remove("products__info--none");
   });
 });
